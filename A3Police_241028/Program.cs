ab37e94f2357d6787baeb08faf34a66ea4a14ad8
Ya-Hsien Wang <109192255+KerkerWang@users.noreply.github.com>
2024-10-28 15:31:59 +0800

    git init

---
+++ A3Police_241028/Program.cs
@@ -0,0 +1,15 @@
+namespace A3Police_241028
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
